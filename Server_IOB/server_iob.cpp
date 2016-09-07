e4533b5c7816227e2fed73a4821b3d052e578f13
kabcode <kabcode@gmail.com>
2016-09-08 00:14:44 +0200

    Create xml document if it doesnt exist

--- Server_IOB/server_iob.cpp
+++ Server_IOB/server_iob.cpp
@@ -22,6 +22,7 @@ Server_IOB::Server_IOB(QWidget *parent)	: QMainWindow(parent)
 	mClientList = loadXMLDocument(mFileName);
 
 
+	
 	// create UI
 	ui.setupUi(this);
 } // END constructor
@@ -44,14 +45,22 @@ QDomDocument Server_IOB::loadXMLDocument(QString file)
 		QFile file(mFileName);
 		if (file.open(QIODevice::WriteOnly))
 		{
+			// create the base structure for xml file
+			QXmlStreamWriter xmlWriter(&file);
+			xmlWriter.setAutoFormatting(true);
+			xmlWriter.writeStartDocument();
+			xmlWriter.writeStartElement("clientList");
+			xmlWriter.writeEndElement();
+			xmlWriter.writeEndDocument();
 			file.close();
 		}
 		else
 		{
-			// show error message box
+			// show error message box and exit the app
 			QMessageBox messageBox;
 			messageBox.critical(0, "Error", "Not allowed to create file!");
 			messageBox.setFixedSize(500, 200);
+			QApplication::exit(EXIT_FAILURE);
 		}
 	}
 	
