d4ca2a4d809157213793b8addd4ae8ca2af30d4b
kabcode <kabcode@gmail.com>
2016-11-21 22:59:18 +0100

    Add client class

---
+++ Server_IOB/client.cpp
@@ -0,0 +1,34 @@
+#include "client.h"
+
+
+Client::Client(QObject *parent)
+	: QObject(parent),
+	name(""),
+	location(""),
+	phone(""),
+	notes("")
+{
+	uuid = QUuid::createUuid();
+	status = STATUS::ABSENT;
+}
+
+Client::~Client()
+{
+}
+
+inline void Client::setName(QString name)		  {	this->name = name;}
+inline void Client::setLocation(QString location) { this->location = location; }
+inline void Client::setPhone(QString phone)		  { this->phone = phone; }
+inline void Client::setNotes(QString notes)		  { this->notes = notes; }
+inline void Client::setStatus(int status)		  { this->status = status; }
+inline void Client::setLastUpdateDateTime()		  {	this->lastUpdateDateTime = QDateTime::currentDateTime();}
+inline void Client::setWebsocket(QWebSocket socket) { this->websocket = socket;  }
+
+inline QUuid	 Client::getUuid() { return uuid; }
+inline QString   Client::getName() { return name; };
+inline int		 Client::getStatus() { return status; };
+inline QString   Client::getLocation() { return location; };
+inline QString   Client::getPhone() { return phone; };
+inline QString	 Client::getNotes() { return notes; };
+inline QDateTime Client::getLastUpdateDateTime() { return lastUpdateDateTime; };
+
---
+++ Server_IOB/client.h
@@ -0,0 +1,53 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QObject>
+#include <QUuid>
+#include <QDateTime>
+#include <QWebsocket>
+
+class Client : public QObject
+{
+	Q_OBJECT
+
+public:
+	Client(QObject *parent);
+	~Client();
+
+	enum STATUS
+	{
+		AVAILABE,
+		BUSY,
+		ABSENT
+	};
+
+private:
+	QUuid uuid;
+	QString name;
+	int		status;
+	QString location;
+	QString phone;
+	QString notes;
+
+	QWebSocket websocket;
+	QDateTime lastUpdateDateTime;
+
+	// getter and setter functions
+	void Client::setName(QString);
+	void Client::setStatus(int);
+	void Client::setLocation(QString);
+	void Client::setPhone(QString);
+	void Client::setNotes(QString);
+	void Client::setLastUpdateDateTime();
+	void Client::setWebsocket(QWebSocket);
+
+	QUuid	Client::getUuid();
+	QString Client::getName();
+	int		Client::getStatus();
+	QString	Client::getLocation();
+	QString	Client::getPhone();
+	QString	Client::getNotes();
+	QDateTime Client::getLastUpdateDateTime();
+};
+
+#endif // CLIENT_H
